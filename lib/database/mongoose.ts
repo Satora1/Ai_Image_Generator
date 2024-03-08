952b06b76b444ea41d6d8fb48cd27d5828e05ccb
Jakub Satora <kubasatora@wp.pl>
2024-03-08 23:42:08 +0100

    DB issue

--- lib/database/mongoose.ts
+++ lib/database/mongoose.ts
@@ -1,23 +1,39 @@
-import mongoose, { Mongoose } from "mongoose";
-const MONGODB_URL = process.env.MONGODB_URL
+import mongoose, { Mongoose } from 'mongoose';
+
+const MONGODB_URL = process.env.MONGODB_URL;
 
 interface MongooseConnection {
-    conn: Mongoose | null;
-    promise: Promise<Mongoose> | null;
+  conn: Mongoose | null;
+  promise: Promise<Mongoose> | null;
 }
 
 let cached: MongooseConnection = (global as any).mongoose
-if (!cached) {
-    cached = (global as any).mongoose = { conn: null, promise: null }
+
+if(!cached) {
+  cached = (global as any).mongoose = { 
+    conn: null, promise: null 
+  }
 }
+
 export const connectToDatabase = async () => {
-    if (cached.conn) return cached.conn;
-
-    if (!MONGODB_URL) throw new Error('Missing MDB database');
-
-    cached.promise = cached.promise || mongoose.connect
-        (MONGODB_URL, { dbName: " IMAGE_AI", bufferCommands: false })
-
-    cached.conn = await cached.promise;
+  if(cached.conn) {
+    console.log('Using cached connection');
     return cached.conn;
+  }
+
+  if(!MONGODB_URL) {
+    throw new Error('Missing MONGODB_URL');
+  }
+
+  console.log('Connecting to the database...');
+
+  cached.promise = mongoose.connect(MONGODB_URL, { 
+    dbName: 'image_ai', bufferCommands: false 
+  });
+
+  cached.conn = await cached.promise;
+
+  console.log('Connected to the database');
+
+  return cached.conn;
 }
